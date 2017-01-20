b0914dccad74ed04988c9929251b4cdaee1cc6c1
devpro <devpro7200@gmail.com>
2017-01-20 20:08:24 +0530

    aditya
    user module is added

--- application/helpers/layout_helper.php
+++ application/helpers/layout_helper.php
@@ -13,4 +13,12 @@ function _vendorLayout($filename,$data=null)
 	   $obj->load->view($filename,$data);
 	   $obj->load->view("common/footer");
 }
+function _userLayout($filename,$data=null)
+{
+	   $obj=& get_instance();
+	   $obj->load->view("common/header");
+	   $obj->load->view($filename,$data);
+	   $obj->load->view("common/footer");
+}
+
 ?>
---
+++ application/modules/user/views/common/sidebar.php
@@ -0,0 +1,21 @@
+ <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="<?php echo site_url();?>/user/">Dashboard</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="<?php echo site_url();?>/user/viewProfile"><i class="fa fa-laptop"></i>Profile</a></li>
+                  <li><a><i class="fa fa-home"></i>Booking status<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="#">Current booking</a></li>
+                      <li><a href="#">All booking</a></li>
+					</ul>
+                  </li>
+				</ul>
+              </div>
+            </div>
+            <!-- /sidebar menu -->
